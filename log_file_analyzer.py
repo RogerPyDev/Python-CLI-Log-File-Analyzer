f0411cbe7b6249103fea2f990d87bbb15acf0e6f
Roger Grajales <70453253+RogerPyDev@users.noreply.github.com>
2024-12-05 07:06:59 -0500

    Update log_file_analyzer.py
    analyze_log function added

--- log_file_analyzer.py
+++ log_file_analyzer.py
@@ -1,2 +1,26 @@
 import argparse
 import re
+
+
+def analyze_log(file_path, pattern):
+
+    try:
+        with open(file_path, 'r') as log_file:
+            print(f"\nAnalizando el archivo: {file_path}")
+            regex = re.compile(pattern)
+            matches = 0
+
+            for line_number, line in enumerate(log_file, start=1):
+                if regex.search(line):
+                    matches += 1
+                    print(f"Linea {line_number}: {line.strip()}")
+                    matches += 1
+
+            if matches == 0:
+                print("No se encontraron coincidencias con el patrón.")
+            else:
+                print(f"\nSe encontraron {matches} lineas que coinciden con el patrón.")
+    except FileNotFoundError:
+        print(f"\nError: El archivo '{file_path}' no se encontro.")
+    except Exception as e:
+        print(f"\nOcurrió un error: {e}")
